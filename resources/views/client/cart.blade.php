069876d1cdbddb2dd795ef134c454656999997ad
phamquanglinhdev <phamquanglinhdev@gmail.com>
2021-12-08 23:20:35 +0700

    fix bug

---
+++ resources/views/client/cart.blade.php
@@ -0,0 +1,18 @@
+@extends("layout.client")
+@section("banner")
+    <div class="page-heading about-page-heading" id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="inner-content">
+                        <h2>Giỏ sách của bạn</h2>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section("content")
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[\App\Http\Controllers\IndexController::class,"index"])->name("index");
-Route::get("/products/{category?}/{page?}",[\App\Http\Controllers\ProductController::class,"index","category","page"])->where(["category","page"])->name("products");
-Route::get("/product/{slug}",[\App\Http\Controllers\ProductController::class,"show","slug"])->where(["slug"])->name("product");
-Route::get("/about",[\App\Http\Controllers\AboutController::class,"index"])->name("about");
-Route::get("/contact",[\App\Http\Controllers\ContactController::class,"index"])->name("contact");
+Route::get("/products/{category?}/{page?}",[ProductController::class,"index","category","page"])->where(["category","page"])->name("products");
+Route::get("/product/{slug}",[ProductController::class,"show","slug"])->where(["slug"])->name("product");
+Route::get("/about",[AboutController::class,"index"])->name("about");
+Route::get("/contact",[ContactController::class,"index"])->name("contact");
+Route::get("/cart",[CartController::class,"index"])->name("contact");
