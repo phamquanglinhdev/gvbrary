7d4c938f6dd8a7406731d690957c561b8c3be18a
phamquanglinhdev <phamquanglinhdev@gmail.com>
2021-12-11 00:19:21 +0700

    fix middleware

---
+++ app/Http/Middleware/CheckUserMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckUserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(backpack_auth()->check()){
+            return $next($request);
+        }else{
+            return redirect("/admin/login");
+        }
+
+    }
+}
